1a2bdbd18e214ce8ee7ffc3a445f8d2a61051cc9
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-10-31 22:34:52 +0200

    3. Тестирование репозиториев [#504862 #519566]

---
+++ src/test/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepositoryTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.dreamjob.repository;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.configuration.DatasourceConfiguration;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.File;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Properties;
+
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class Sql2oCandidateRepositoryTest {
+    private static Sql2oCandidateRepository sql2oCandidateRepository;
+
+    private static Sql2oFileRepository sql2oFileRepository;
+
+    private static File file;
+
+    @BeforeAll
+    public static void initRepositories() throws Exception {
+        var properties = new Properties();
+        try (var inputStream = Sql2oCandidateRepositoryTest.class.getClassLoader().getResourceAsStream("connection.properties")) {
+            properties.load(inputStream);
+        }
+        var url = properties.getProperty("datasource.url");
+        var username = properties.getProperty("datasource.username");
+        var password = properties.getProperty("datasource.password");
+
+        var configuration = new DatasourceConfiguration();
+        var datasource = configuration.connectionPool(url, username, password);
+        var sql2o = configuration.databaseClient(datasource);
+
+        sql2oCandidateRepository = new Sql2oCandidateRepository(sql2o);
+        sql2oFileRepository = new Sql2oFileRepository(sql2o);
+
+        file = new File("test2", "test2");
+        sql2oFileRepository.save(file);
+    }
+
+    @AfterAll
+    public static void deleteFile() {
+        sql2oFileRepository.deleteById(file.getId());
+    }
+
+    @AfterEach
+    public void clearCandidates() {
+        var candidates = sql2oCandidateRepository.findAll();
+        for (var candidate : candidates) {
+            sql2oCandidateRepository.deleteById(candidate.getId());
+        }
+    }
+
+    @Test
+    public void whenSaveThenGetSame() {
+        var creationDate = now().truncatedTo(ChronoUnit.MINUTES);
+        var candidate = sql2oCandidateRepository.save(new Candidate(0, "name", "description", creationDate, 1, file.getId()));
+        var savedCandidate = sql2oCandidateRepository.findById(candidate.getId()).get();
+        assertThat(savedCandidate).usingRecursiveComparison().isEqualTo(candidate);
+    }
+}
